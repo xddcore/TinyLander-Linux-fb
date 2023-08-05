a96012aa76104283772c3f31e326a9a469ecd80a
Chengsen Dong <1034029664@qq.com>
2023-08-05 12:33:59 +0100

    update

--- gameinterface.cpp
+++ gameinterface.cpp
@@ -181,17 +181,13 @@ int Keyboard_Init()
 
 void Keyboard_Event()
 {
-    int fd = open(KEYBOARD_DEVICE, O_RDONLY);
-    if (fd == -1) {
-      perror("Error opening keyboard device");
-    }
     struct input_event ev;
 
     // Change file descriptor to non-blocking
-    int flags = fcntl(fd, F_GETFL, 0);
-    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    int flags = fcntl(key_board_fb, F_GETFL, 0);
+    fcntl(key_board_fb, F_SETFL, flags | O_NONBLOCK);
 
-    ssize_t n = read(fd, &ev, sizeof(struct input_event));
+    ssize_t n = read(key_board_fb, &ev, sizeof(struct input_event));
 
     if (n == sizeof(struct input_event)) {
             if (ev.type == EV_KEY) {
@@ -227,8 +223,6 @@ void Keyboard_Event()
       D_key_pressed=0;
       Space_key_pressed=0;
     }
-
-    close(fd);
 
 
 }
